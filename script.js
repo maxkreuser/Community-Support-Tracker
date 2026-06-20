1af7a53a00420570e9fd11a9fb7ef819e3340408
Maximilian Kreuser <mkreuser@rrc.ca>
2026-06-20 15:39:59 -0500

    feat: initiated event listener on the form. refactor: change rating input to radio buttons styled to star shapes.

---
+++ script.js
@@ -0,0 +1,10 @@
+const volunteerForm = document.getElementById("volunteer-form")
+
+volunteerForm.addEventListener("submit", addHours)
+
+function addHours() {
+    const name = document.getElementById("volunteer-charity-name")
+    const hours = document.getElementById("hours")
+    const date = document.getElementById("volunteer-date")
+    const rating = document.getElementById("star-rating")
+}
